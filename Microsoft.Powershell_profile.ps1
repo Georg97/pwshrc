47b406ef4fbc37b45e40c65a45486d15eb5375de
Georg Hirsch <g.hirsch@imrecycling.de>
2023-11-21 16:21:15 +0100

    update

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -25,3 +25,22 @@ Set-Alias termc ConfigureWindowsTerminal
 Set-Alias gitll GitLogOneline
 Set-Alias gitl GitLog
 Set-Alias restart-komorebic RestartKomorebi
+
+# $Env:KOMOREBI_CONFIG_HOME = 'C:\Users\g.hirsch\.config\komorebi'
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# new powershell version path
+$pwsh_executable = 'C:\Program Files\PowerShell\7\pwsh.exe'
+if (Test-Path -Path $pwsh_executable) {
+    $pwsh_executable
+}
+
